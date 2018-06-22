c6fb898e36b5a28cb423ddcd3c86e87fd1092afc
nd-02110114 <nd.12021218@gmail.com>
2018-06-22 17:01:08 +0900

    :sparkles: create Energy show script for OUTCAR

--- utils/energy.py
+++ utils/energy.py
@@ -1,5 +1,3 @@
-"""VASP OUTCARからエネルギーを抽出"""
-
 #!/usr/bin/python
 # coding: UTF-8
 
@@ -14,15 +12,31 @@ args = sys.argv
 class Energy():
     def __init__(self, content):
         self.content = content
+        self.result = []
 
-    def print(self):
-        print(self.content)
+    def getEnergyAndIterationNumber(self):
+        tmp_content = self.content
+        for line in tmp_content:
+            if line.find("Iteration") != -1:
+                self.result.append(line.replace("-", ""))
+
+            if line.find("TOTEN") != -1:
+                self.result.append(line)
+
+    def show(self):
+        output = ""
+        tmp_result = self.result
+        for line in tmp_result:
+            output += line
+
+        print(output)
 
 
 
 if __name__ == '__main__':
     with open(args[1], 'r') as f:
-        outcar = f.read()
+        lines = f.readlines()
 
-    energy = Energy(outcar)
-    energy.print()
+    energy = Energy(lines)
+    energy.getEnergyAndIterationNumber()
+    energy.show()
